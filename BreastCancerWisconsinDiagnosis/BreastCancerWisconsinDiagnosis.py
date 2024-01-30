bffd97857aa542b1f4ca3e92e4a0d0e42f9d9e14
c0r2a-lab <richardjkennedy9699@gmail.com>
2024-01-30 13:18:13 +0800

    update: f-b propagation

--- BreastCancerWisconsinDiagnosis/BreastCancerWisconsinDiagnosis.py
+++ BreastCancerWisconsinDiagnosis/BreastCancerWisconsinDiagnosis.py
@@ -39,3 +39,19 @@ def initialize_weights_and_bias(dimension):
     b = 0.0
     return w, b
 
+# z = np.dot(w.T, x_train) + b
+def sigmoid(z):
+    y_head = 1/(1 + np.exp(-z))
+    return y_head
+
+def forward_backward_propagation(w, b, x_train, y_train):
+    z = np.dot(w.T, x_train) + b
+    y_head = sigmoid(z)
+    loss = -y_train * np.log(y_head) - (1 - y_train) * np.log(1 - y_head)
+    # x_train.shape[1] is for scaling
+    cost = (np.sum(loss)) / x_train.shape[1]
+    # backward propagation
+    derivative_weight = (np.dot(x_train, ((y_head - y_train).T))) / x_train.shape[1]
+    derivative_bias = (np.sum(y_head - y_train) / x_train.shape[1])
+    gradients = {"derivative_weight": derivative_weight, "derivative_bias": derivative_bias}
+    return cost, gradients
